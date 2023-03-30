3bd5986e50c9772ead4b240fbbe7801cea7c39c6
ahmedsheblwafa <ahmedsheblwafa@gmail.com>
2023-03-30 15:52:33 +0200

    add fonts

--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import "./Navbar.styles.scss"
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+        <h1>Navbar</h1>
+        <p>hello from Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis tempore ipsam id optio, porro excepturi soluta temporibus magnam asperiores dicta provident autem voluptate perferendis amet explicabo? Adipisci eius sed debitis?</p>
+    </div>
   )
 }
