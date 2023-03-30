75441f3a5495c1a72cfbc1819943394495b2aa56
hagar7 <hagar_s_shamed@hotmail.com>
2023-03-30 20:13:34 +0200

    banner

--- src/components/Company/Company.component.jsx
+++ src/components/Company/Company.component.jsx
@@ -4,10 +4,7 @@ export default function Company() {
         <section id="company">
             <h2>Solvix</h2>
             <p>
-                Since 2009, our company has a successful business experience and knowledge in the sector of chemical
-                trading and manufacturing.Our mission is to pursue global developments and innovations,to practice the
-                best options, to respect the environment and human health,to be the leading firm in the sector with the
-                principle of reliability.
+            knowledge in the sector of chemical trading and manufacturing
             </p>
         </section>
     )
