879727fae2ff3121d1fc584175148c043d3ccd91
ahmedsheblwafa <ahmedsheblwafa@gmail.com>
2023-03-30 19:42:19 +0200

    add company section

--- src/components/Company/Company.component.jsx
+++ src/components/Company/Company.component.jsx
@@ -1,5 +1,14 @@
-import React from 'react'
-
+import './Company.styles.scss'
 export default function Company() {
-    return <div id='company'>Company</div>
+    return (
+        <section id="company">
+            <h2>Solvix</h2>
+            <p>
+                Since 2009, our company has a successful business experience and knowledge in the sector of chemical
+                trading and manufacturing.Our mission is to pursue global developments and innovations,to practice the
+                best options, to respect the environment and human health,to be the leading firm in the sector with the
+                principle of reliability.
+            </p>
+        </section>
+    )
 }
--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -2,67 +2,33 @@ import React from 'react'
 import style from './Navbar.module.scss'
 
 export default function Navbar() {
-  return (
-    <div className={`${style.navbar}`}>
-      <div className="container">
-        <div className={`${style.content}`}>
-        <div className={`${style.img}`}>
-          <img src="images/logo.png" alt="logo" />
+    return (
+        <div className={`${style.navbar}`}>
+            <div className="container">
+                <div className={`${style.content}`}>
+                    <div className={`${style.img}`}>
+                        <img src="images/logo.png" alt="logo" />
+                    </div>
+
+                    <div className={`${style.listItems}`}>
+                        <a href="/" className={({ isActive }) => (isActive ? ' nav-link hoverClr' : 'nav-link')}>
+                            <span>Home</span>
+                        </a>
+                        <a href="/" className={({ isActive }) => (isActive ? ' nav-link hoverClr' : 'nav-link')}>
+                            <span>Company</span>
+                        </a>
+                        <a href="/" className={({ isActive }) => (isActive ? ' nav-link hoverClr' : 'nav-link')}>
+                            <span>Products</span>
+                        </a>
+                        <a href="/" className={({ isActive }) => (isActive ? ' nav-link hoverClr' : 'nav-link')}>
+                            <span>Services</span>
+                        </a>
+                        <a href="/" className={({ isActive }) => (isActive ? ' nav-link hoverClr' : 'nav-link')}>
+                            <span>Contact</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-
-         <div className={`${style.listItems}`}>
-          <a
-            href="/"
-            className={({ isActive }) =>
-              isActive ? " nav-link hoverClr" : "nav-link"
-            }
-          >
-            <span>Home</span>
-          </a>
-         <a
-            href="/" id='company'
-            className={({ isActive }) =>
-              isActive ? " nav-link hoverClr" : "nav-link"
-            }
-          >
-            <span>Company</span>
-          </a>
-         <a
-            href="/"
-            className={({ isActive }) =>
-              isActive ? " nav-link hoverClr" : "nav-link"
-            }
-          >
-            <span>Products</span>
-          </a>
-        <a
-            href="/"
-            className={({ isActive }) =>
-              isActive ? " nav-link hoverClr" : "nav-link"
-            }
-          >
-            <span>Services</span>
-          </a>
-          <a
-            href="/"
-            className={({ isActive }) =>
-              isActive ? " nav-link hoverClr" : "nav-link"
-            }
-          >
-            <span>Contact</span>
-          </a>
-        </div> 
-        </div>
-
-
-
-
-
-
-
-
-      </div>
-
-    </div>
-  )
+    )
 }
